f72d20e2aa7cc92c5c101c3a1a3e8d4b7e315a89
yeyuhang <yeyuhang@appicplay.com>
2021-12-01 15:05:32 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2021-11-22 11:00:09
- * @LastEditTime : 2021-11-29 17:16:16
+ * @LastEditTime : 2021-12-01 15:05:11
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /new-chat/vite.config.js
@@ -24,34 +24,34 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     proxy: {
-        '/api': {
-            target: 'http://47.93.229.170:3000',
-            // target: 'http://localhost:3000',
-            changeOrigin: true,
-            rewrite: path => path.replace(/^\/api/, '')
-        }
+      '/api': {
+        target: 'http://47.93.229.170:3000',
+        // target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
     }
   },
   base: './',
   build: {
-      assetsDir: 'assets',
-      //设置为 false 来禁用将构建后的文件写入磁盘
-      write: true,
-      //默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
-      emptyOutDir: true,
-      //启用/禁用 brotli 压缩大小报告
-      brotliSize: true,
-      //chunk 大小警告的限制
-      chunkSizeWarningLimit: 1000,
-      // 移除console
-      minify: 'terser',
-      terserOptions: {
-          compress: {
-              //生产环境时移除console
-              drop_console: true,
-              drop_debugger: true,
-          },
+    assetsDir: 'assets',
+    //设置为 false 来禁用将构建后的文件写入磁盘
+    write: true,
+    //默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
+    emptyOutDir: true,
+    //启用/禁用 brotli 压缩大小报告
+    brotliSize: true,
+    //chunk 大小警告的限制
+    chunkSizeWarningLimit: 1000,
+    // 移除console
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
       },
+    },
   },
   //调整控制台输出的级别 'info' | 'warn' | 'error' | 'silent'
   logLevel: 'info',
